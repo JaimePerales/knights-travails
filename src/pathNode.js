b018e45186f39090c0f94b267891ee959539f794
Jaime <jaimeperales123@gmail.com>
2023-04-03 07:57:54 -0500

    Create path node class

---
+++ src/pathNode.js
@@ -0,0 +1,11 @@
+
+/**
+ * Represents a node in the path taken to reach desired
+ * Once Square is reached, push square and parent node to 
+ * stack
+ */
+class PathNode {
+    constructor(parentNode){
+        this.parentNode = parentNode;
+    }
+}
